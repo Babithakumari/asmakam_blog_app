d414d6a2cc58fe3ad412ece8312e7cc9e59c940f
Babithakumari <babithakumari120@gmail.com>
2023-06-17 09:07:57 +0530

    layout changes

--- blog_app/models.py
+++ blog_app/models.py
@@ -9,9 +9,11 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=255, unique=True)
     subtitle = models.CharField(max_length=255, blank=True)
     slug = models.SlugField(max_length=255, unique=True)
+    image = models.ImageField(upload_to='images/', default=None, blank=True, null=True)
     body = models.TextField()
     meta_description = models.CharField(max_length=150, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
     publish_date = models.DateTimeField(blank=True, null=True)
     published = models.BooleanField(default=False)
+
